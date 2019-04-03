243502e79caf0bcdf9c0e32e5dff06705eedb2d9
fengyu <fengyu@163.com>
2019-04-03 16:12:00 +0800

    单例模式

--- src/main/java/DesignPatterns/Single/hungry/SingleHungryTest.java
+++ src/main/java/DesignPatterns/Single/hungry/SingleHungryTest.java
@@ -7,30 +7,29 @@ import java.util.concurrent.CountDownLatch;
  * 〈〉
  *
  * @author fy
- * @create 2018/12/19
+ * @date 2018/12/19
  */
 public class SingleHungryTest {
 
 
 
     public static void main(String[] args) {
-        System.out.println("hahahhah");
-//        final int count = 100;
-//        CountDownLatch countDownLatch = new CountDownLatch(count);
-//        for (int i = 0; i < count; i++) {
-//            new Thread() {
-//                @Override
-//                public void run() {
-//                    SingleHungry.getInstance();
-//                }
-//            }.start();
-//            countDownLatch.countDown();
-//        }
-//
-//        try {
-//            countDownLatch.await();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        final int count = 100;
+        CountDownLatch countDownLatch = new CountDownLatch(count);
+        for (int i = 0; i < count; i++) {
+            new Thread() {
+                @Override
+                public void run() {
+                    SingleHungry.getInstance();
+                }
+            }.start();
+            countDownLatch.countDown();
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
